b265529d46a588f6c4b5903ca436fdc0c2df643c
Godwinvimal <63697212+Godwinvimal@users.noreply.github.com>
2021-04-20 13:06:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "180px";
+}
+
+val = document.getElementById("mySidenav")
+
+function closeNav() {
+  val.style.width = "0";
+}
 
